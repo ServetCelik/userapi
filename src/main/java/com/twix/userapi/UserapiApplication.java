9c73b4be59b2af146b8b03bd2bc56defa57739de
I489394 <servetcan.celik@student.fontys.nl>
2024-04-25 23:51:15 +0200

    eureka server commented out

--- src/main/java/com/twix/userapi/UserapiApplication.java
+++ src/main/java/com/twix/userapi/UserapiApplication.java
@@ -2,7 +2,6 @@ package com.twix.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +12,7 @@ public class UserapiApplication {
 		SpringApplication.run(UserapiApplication.class, args);
 	}
 	@Bean
-	@LoadBalanced
+	//@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
