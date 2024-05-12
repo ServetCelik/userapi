7ffed47c6052f2afeec12bc778a92d66aff8e326
I489394 <servetcan.celik@student.fontys.nl>
2024-05-12 09:01:36 +0200

    optimize imports

--- src/main/java/com/twix/userapi/business/exceptions/UserNameAlreadyExistsException.java
+++ src/main/java/com/twix/userapi/business/exceptions/UserNameAlreadyExistsException.java
@@ -1,8 +1,5 @@
 package com.twix.userapi.business.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 public class UserNameAlreadyExistsException extends RuntimeException {
     public UserNameAlreadyExistsException(String message) {
         super(message);
--- src/main/java/com/twix/userapi/business/exceptions/UserNotExistException.java
+++ src/main/java/com/twix/userapi/business/exceptions/UserNotExistException.java
@@ -1,8 +1,5 @@
 package com.twix.userapi.business.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 public class UserNotExistException extends RuntimeException {
     public UserNotExistException(String message) {
         super(message);
